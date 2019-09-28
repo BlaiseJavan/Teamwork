138ac15666bb8b694dddec118864fdb81376e0f4
BlaiseJavan <blaiseirakoze50@gmail.com>
2019-09-28 19:19:17 +0200

    [ft-168822421] write a new article form

--- UI/js/script.js
+++ UI/js/script.js
@@ -1,1 +1,22 @@
-alert(ok);
+// report button                      
+const report = document.querySelectorAll(".report a");
+const deleteBtn = document.querySelectorAll(".delete-article");
+
+// report
+for (let index = 0; index < report.length; index++) {
+    report[index].onclick = function() {
+        report[index].style.background  =  "rgb(231, 81, 81)";
+        report[index].innerHTML  =  "inapro";
+    };   
+}          
+
+// article delete alert
+function deleteArticle() {
+    alert('are you sure you want to delete?');
+}
+
+deleteBtn.forEach(element => {
+    element.addEventListener('click', deleteArticle);
+});
+
+
