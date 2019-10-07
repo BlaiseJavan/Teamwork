58b99e25f3e5741b424f45883ef9be5562ba0f87
BlaiseJavan <blaiseirakoze50@gmail.com>
2019-10-07 15:31:35 +0200

    chore(auth): re-write auth middleware[#168982483]

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,17 +3,22 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// eslint-disable-next-line consistent-return
 const auth = (req, res, next) => {
-  const { token } = req.headers;
-  const verifyToken = jwt.verify(token, process.env.secret);
-  if (verifyToken) {
-    req.user = verifyToken;
-    next();
+  try {
+    const { token } = req.headers;
+    const verifyToken = jwt.verify(token, process.env.secret);
+
+    if (verifyToken) {
+      req.user = verifyToken;
+      next();
+    }
+  } catch (error) {
+    return res.status(403).json({
+      status: 403,
+      error: 'authantication is not valid',
+    });
   }
-  return res.status(403).json({
-    status: 403,
-    error: 'authantication is not valid',
-  });
 };
 
 export default auth;
